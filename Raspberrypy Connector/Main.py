7d9ab933737a6c5fbaaed10c6b5e08f0652d1ac1
Matt <mahdi.rajaee@ymail.com>
2025-02-10 01:17:09 +0100

    update on MS_TimeSeriesDbConnector

--- Raspberrypy Connector/Main.py
+++ Raspberrypy Connector/Main.py
@@ -1,11 +1,14 @@
-from simulation.sensor_simulator import SensorSimulator
-from config import *
+from Simulation.sensor_simulator import SensorSimulator
+from Config import *
 
 if __name__ == "__main__":
     try:
         # Initialize the sensor simulator
         simulator = SensorSimulator(DEVICE_ID, BROKER_ADDRESS, BROKER_PORT)
         simulator.start()
+        
+        TOPIC = "sensor/data"  # Define your MQTT topic
+
 
         # Start simulating sensor readings
         simulator.simulate_sensors()
